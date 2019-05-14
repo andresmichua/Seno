14d9dad7792e281e70836c78689a22871bb0e3e6
andresmichua <andriu.rost@gmail.com>
2019-05-13 20:19:28 -0500

    implementacion de metodo calcula numeros impares de 1 al 13

--- src/seno/Seno.java
+++ src/seno/Seno.java
@@ -9,12 +9,15 @@ public class Seno {
         
         System.out.println("facotrial de 0 "+factorial(0));
         for(int i=1;i<=13;i+=2){
-        System.out.println("factorial de "+i+" "+factorial(i));
-        
-            System.out.println(numeroImpares(2));
-            
-            System.out.println("El calculo del exponente es "+calculaExponente(2, 5));
+        System.out.println("factorial de "+i+" "+factorial(i)); 
         }
+        System.out.println(numeroImpares(2));
+            
+        System.out.println("El calculo del exponente es "+calculaExponente(2, 5));
+        
+        imprimeImpares(13);
+        
+        
     }
     
     public static long factorial(int n) {
@@ -36,6 +39,12 @@ public class Seno {
         int resultado=0;
         resultado = (int)Math.pow(base, exponente);
         return resultado;
+    }
+    
+    public static void imprimeImpares(int a){
+        int i;
+        for (i=1;i<=a;i+=2)
+            System.out.println(i);
     }
     
 }
